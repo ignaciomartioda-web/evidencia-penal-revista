ee53fbe52f27a8645f9a9347ddb84252ce2a2f37
ignaciomartioda-web <ignaciomartioda@gmail.com>
2026-05-07 12:14:27 -0300

    Full sync of styles and config

---
+++ actualizar.bat
@@ -0,0 +1,16 @@
+@echo off
+echo ========================================
+echo   ACTUALIZANDO REVISTA EVIDENCIA PENAL
+echo ========================================
+echo.
+
+git add .
+git commit -m "Mejoras en el diseno y fluidos dinÃ¡micos"
+git push -u origin main
+
+echo.
+echo ========================================
+echo   Â¡EXITO! Tu web se estÃ¡ actualizando...
+echo   Revisa el link en 30 segundos.
+echo ========================================
+pause
