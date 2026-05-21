764929a8036f930a26eac9fa98705f7484059aea
ignaciomartioda-web <ignaciomartioda@gmail.com>
2026-05-21 10:29:18 -0300

    Actualizar revista con convocatorias y cambios locales

--- actualizar.bat
+++ actualizar.bat
@@ -5,12 +5,12 @@ echo ========================================
 echo.
 
 git add .
-git commit -m "Mejoras en el diseno y fluidos dinÃ¡micos"
+git commit -m "Mejoras en el diseno y fluidos dinámicos"
 git push -u origin main
 
 echo.
 echo ========================================
-echo   Â¡EXITO! Tu web se estÃ¡ actualizando...
+echo   ¡EXITO! Tu web se está actualizando...
 echo   Revisa el link en 30 segundos.
 echo ========================================
 pause
